a1b377c45c3290daf0d3bee6d14eb6f5139a68a8
nayonptk <jahedulptk@gmail.com>
2018-12-07 11:58:11 +0600

    init version

---
+++ js/jquery.js
@@ -0,0 +1,10 @@
+(function($) {
+	
+	
+	$(".mobile-manu").slicknav();
+	
+
+	
+	
+
+})(jQuery)
---
+++ js/script.js
@@ -0,0 +1,16 @@
+(function($){
+	
+	
+	
+			// manuber & mainmanu
+	
+		$('.header-manu').click(function(){
+		
+		$('.main-manu').slideToggle(1000);
+		
+		});
+		
+
+})(jQuery);
+
+
